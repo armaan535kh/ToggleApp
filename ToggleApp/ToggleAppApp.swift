d736857b3e8dd996d953b3f793dd897734b46781
armaan535kh <kh.armaan535@gmail.com>
2024-09-11 06:47:51 +0530

    Initial Commit

---
+++ ToggleApp/ToggleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleAppApp.swift
+//  ToggleApp
+//
+//  Created by Armaan Khan  on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ToggleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
